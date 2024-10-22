27e04b3f8b02d9d87a2f3640705230767634a0fd
lucasjdutra <lucasjdc90@gmail.com>
2024-10-22 09:41:17 -0300

    commit 7: listar os livros cadastrados em uma tabela

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrl: './books.component.css'
+  styleUrls: ['./books.component.css'],
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  books: Book[] = [];
+  newBook: Book = { id: 0, title: '', author: '', genre: '', price: 0 };
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Book[]>('http://localhost:3000/books').subscribe((data) => {
+      this.books = data;
+    });
+  }
+
+  removeBook(id: number): void {
+    this.http.delete(`http://localhost:3000/books/${id}`).subscribe(() => {
+      this.books = this.books.filter((book) => book.id !== id);
+    });
+  }
+
+  addBook(): void {
+    this.http
+      .post<Book>('http://localhost:3000/books', this.newBook)
+      .subscribe((book) => {
+        this.books.push(book);
+        this.newBook = {
+          id: 0,
+          title: '',
+          author: '',
+          genre: '',
+          price: 0,
+        };
+      });
+  }
 }
